7baab58501f84c5a7434ab61aef2df71df31bf2e
VicenteContrerasUfro <v.contreras12@ufromail.cl>
2024-09-11 09:57:00 -0300

    commit 1

---
+++ src/main/java/org/example/JuegoMatriz.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class JuegoMatriz {
+    public String[][] matriz;
+
+
+
+    public JuegoMatriz() {
+        matriz = new String[10][10];
+        matriz[0][0] = "";
+    }
+
+}
+
---
+++ src/test/java/JuegoMatrizTeat.java
@@ -0,0 +1,2 @@
+public class JuegoMatrizTeat {
+}
